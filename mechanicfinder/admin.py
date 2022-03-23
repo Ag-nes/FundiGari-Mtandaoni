31376998aeffa4098658a20f8f9bfe30059c8cbc
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-03-24 00:50:42 +0300

    register models

--- mechanicfinder/admin.py
+++ mechanicfinder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Business,User,Client,Garage,Feedback
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Client)
+admin.site.register(Garage)
+admin.site.register(Business)
+admin.site.register(Feedback)
